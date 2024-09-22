487aea1a8f28b46cc235ba94e55c89dbb1bdb486
Shohruh <sattorovshoxrux3009@gmail.com>
2024-09-22 20:50:41 +0500

    Create

---
+++ server.js
@@ -0,0 +1,23 @@
+const TelegramBot = require('node-telegram-bot-api');
+
+const token = '7616988037:AAEv5iazl79YRZeH_VO7r1qptFqXb33pRFk';
+const bot = new TelegramBot(token, { polling: true });
+
+bot.on('message', (msg) => {
+  const chatId = msg.chat.id;
+
+  // Tugmalarni yaratish
+  const options = {
+    reply_markup: {
+      keyboard: [
+        ['A', 'B'], // Birinchi qator
+        ['C', 'D'], // Ikkinchi qator
+        [{ text: 'Ortga', callback_data: 'back' }] // Uchinchi qator
+      ],
+      resize_keyboard: true, // Tugmalarni ekranga moslashtirish
+      one_time_keyboard: false // Klaviatura foydalanuvchiga doimiy ko'rinadi
+    }
+  };
+
+  bot.sendMessage(chatId, 'Tugmalarni tanlang:', options);
+});
